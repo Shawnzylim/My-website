c178901adfde533f3d8345f14994efdcd4a5b7a9
Your Name <you@example.com>
2023-06-08 02:33:25 +0800

    final?

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,11 @@
 import React from "react";
 import CardItem from "./CardItem";
 import "./Cards.css";
+import swgoh from "./swgoh-old.webp";
+import overwatch from "./overwatch.webp";
+import steam from "./steam.jpg";
+import chess from "./chess.webp";
+import LOL from "./LOL.jpg";
 
 function Cards() {
   return (
@@ -11,13 +16,13 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/swgoh-old.webp"
+              src={swgoh}
               text="Join the Dark Side"
               label="Star Wars Galaxy of Heroes"
               path="https://swgoh.gg/p/592983585/"
             />
             <CardItem
-              src="images/overwatch.webp"
+              src={overwatch}
               text="If at first you don't succeed. Blow it up again!"
               label="Overwatch"
               path="https://www.overbuff.com/players/MasterMcJugs-1812"
@@ -25,19 +30,19 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/steam.jpg"
+              src={steam}
               text="Check out my profile"
               label="steam"
               path="https://steamcommunity.com/id/Shawnzylim/"
             />
             <CardItem
-              src="images/chess.webp"
+              src={chess}
               text="I never lose at chess"
               label="chess.com"
               path="https://www.chess.com/member/shawnzylim"
             />
             <CardItem
-              src="images/LOL.jpg"
+              src={LOL}
               text="New Champ = dash"
               label="League of legends"
               path="https://app.mobalytics.gg/lol/profile/sg/molestchen/overview"
